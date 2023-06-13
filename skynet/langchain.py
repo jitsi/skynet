136190e7e5873c307e665c37bea6fcf72551fca3
Avram Tudor <tudor.potecaru@8x8.com>
2023-06-13 11:04:29 +0300

    feat: implement basic CRUD for /summary

--- skynet/langchain.py
+++ skynet/langchain.py
@@ -1,11 +1,39 @@
+import logging
+
 from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class Langchain:
+    def __init__(self):
+        self.chains = {}
+
     def summarize(self, text: str):
+        if not text:
+            return ""
+
         text_splitter = RecursiveCharacterTextSplitter()
         docs = text_splitter.create_documents([text])
         chain = load_summarize_chain(OpenAI(temperature=0), chain_type="map_reduce")
 
         return chain.run(docs)
+
+    def get_summary(self, id: str):
+        memory = self.chains.setdefault(id, ConversationBufferMemory())
+        history = memory.load_memory_variables({}).get("history")
+
+        return self.summarize(history)
+
+    def update_summary(self, id: str, text: str):
+        memory = self.chains.setdefault(id, ConversationBufferMemory())
+        memory.save_context({"input": text }, {"output": ""})
+
+        return memory.load_memory_variables({}).get("history")
+
+    def delete_summary(self, id: str):
+        if (id in self.chains):
+            del self.chains[id]
+            return True
+
+        return False
--- skynet/main.py
+++ skynet/main.py
@@ -5,7 +5,20 @@ from skynet.models.summary import SummaryPayload
 
 app = FastAPI()
 langchain = Langchain()
+version = "v1"
 
-@app.post("/summary")
-def summary(payload: SummaryPayload):
+@app.post(f"/{version}/summary")
+def create_summary(payload: SummaryPayload):
     return langchain.summarize(payload.text)
+
+@app.get(f"/{version}/summary/" + "{id}")
+def get_summary(id: str):
+    return langchain.get_summary(id)
+
+@app.put(f"/{version}/summary/" + "{id}")
+def update_summary(id: str, payload: SummaryPayload):
+    return langchain.update_summary(id, payload.text)
+
+@app.delete(f"/{version}/summary/" + "{id}")
+def delete_summary(id: str):
+    return langchain.delete_summary(id)
