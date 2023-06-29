c4637841966a62e50e467c71749c30c594466fac
Saúl Ibarra Corretgé <saghul@jitsi.org>
2023-06-29 11:30:09 +0200

    misc: simplify code
    It's not necessary to specify the parent class in Python 3.

--- skynet/auth/bearer.py
+++ skynet/auth/bearer.py
@@ -7,14 +7,14 @@ from skynet.auth.jwt import decodeJWT
 
 class JWTBearer(HTTPBearer):
     def __init__(self):
-        super(JWTBearer, self).__init__(auto_error=True)
+        super().__init__(auto_error=True)
 
         self.SSO_PUBKEY = bytes(os.environ.get('SSO_PUBKEY'), 'utf-8')
         self.SSO_ISSUER = os.environ.get('SSO_ISSUER')
         self.SSO_ALGORITHM = os.environ.get('SSO_PUBKEY_ALGORITHM', 'RS256')
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
 
         if credentials and decodeJWT(credentials.credentials, self.SSO_PUBKEY, [self.SSO_ALGORITHM], self.SSO_ISSUER):
             return credentials.credentials
