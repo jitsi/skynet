bd80b567318d61b26d2cfd9e0504a497c54c6675
RazvanP <razvan.purdel@8x8.com>
2023-08-10 12:16:23 +0300

    Convert env types

--- skynet/env.py
+++ skynet/env.py
@@ -12,5 +12,5 @@ asap_pub_keys_url = os.getenv('ASAP_PUB_KEYS_REPO_URL', None)
 if not bypass_auth and not asap_pub_keys_url:
     raise RuntimeError('The ASAP public keys repo url must be set')
 
-llama_n_gpu_layers = os.environ.get('LLAMA_N_GPU_LAYERS', 1 if is_mac else 40)
-llama_n_batch = os.environ.get('LLAMA_N_BATCH', 512)
+llama_n_gpu_layers = int(os.environ.get('LLAMA_N_GPU_LAYERS', 1 if is_mac else 40))
+llama_n_batch = int(os.environ.get('LLAMA_N_BATCH', 512))
