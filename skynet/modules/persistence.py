2735c4eeec5ae12ded70d30515e5a7fbe49a0554
Saúl Ibarra Corretgé <saghul@jitsi.org>
2023-10-05 09:56:19 +0200

    redis: remove unneeded class

--- skynet/modules/persistence.py
+++ skynet/modules/persistence.py
@@ -8,31 +8,27 @@ from skynet.env import (redis_host,
                         redis_db_no,
                         redis_usr,
                         redis_pwd)
-from skynet.logs import get_logger
-
-log = get_logger('skynet.redis')
 
 
-class Redis:
-    def __init__(self):
-        connection_options = {
-            'host': redis_host,
-            'port': redis_port,
-            'socket_connect_timeout': 4000,
-            'decode_responses': True,
-            'ssl': redis_use_tls,
-            'db': redis_db_no,
-            'ssl_cert_reqs': None
-        }
+def initialize_redis():
+    connection_options = {
+        'host': redis_host,
+        'port': redis_port,
+        'socket_connect_timeout': 4000,
+        'decode_responses': True,
+        'ssl': redis_use_tls,
+        'db': redis_db_no,
+        'ssl_cert_reqs': None
+    }
 
-        if redis_use_secrets_manager:
-            aws_client = boto3.client('secretsmanager')
-            redis_aws_pass = aws_client.get_secret_value(SecretId=redis_aws_secret_id)['SecretString']
-            connection_options['password'] = redis_aws_pass
-        else:
-            connection_options['username'] = redis_usr
-            connection_options['password'] = redis_pwd
+    if redis_use_secrets_manager:
+        aws_client = boto3.client('secretsmanager')
+        redis_aws_pass = aws_client.get_secret_value(SecretId=redis_aws_secret_id)['SecretString']
+        connection_options['password'] = redis_aws_pass
+    else:
+        connection_options['username'] = redis_usr
+        connection_options['password'] = redis_pwd
 
-        self.db = redis.Redis(**connection_options)
+    return redis.Redis(**connection_options)
 
-db = Redis().db
+db = initialize_redis()
