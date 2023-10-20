eefc5e0d99e150a5398133a252cec2442fd07524
Saúl Ibarra Corretgé <saghul@jitsi.org>
2023-10-20 10:08:42 +0200

    core: add ability to select which apps Skynet runs

--- skynet/apps/summaries.py
+++ skynet/apps/summaries.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi_versionizer.versionizer import versionize
-from skynet.modules.monitoring import PROMETHEUS_NAMESPACE, PROMETHEUS_SUMMARIES_SUBSYSTEM
 
-from skynet.routers.v1 import router as v1_router
 from skynet.env import enable_metrics
+from skynet.logs import get_logger
+from skynet.modules.monitoring import PROMETHEUS_NAMESPACE, PROMETHEUS_SUMMARIES_SUBSYSTEM
+from skynet.modules.persistence import db
+from skynet.modules.ttt.jobs import start_monitoring_jobs
+from skynet.modules.ttt.summaries import initialize as initialize_summaries
+from skynet.routers.v1 import router as v1_router
+
+
+log = get_logger('skynet.summaries')
 
 app = FastAPI()
 app.include_router(v1_router)
@@ -24,4 +31,14 @@ def root():
     return RedirectResponse(url='v1/docs')
 
 
-__all__ = ['app']
+async def app_startup():
+    initialize_summaries()
+    log.info('Summaries initialized')
+
+    await db.initialize()
+    log.info('Persistence initialized')
+
+    start_monitoring_jobs()
+
+
+__all__ = ['app', 'app_startup']
