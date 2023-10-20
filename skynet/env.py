540255d16a0ba307ffc8a64829ecfb7816377f87
Saúl Ibarra Corretgé <saghul@jitsi.org>
2023-10-20 09:50:42 +0200

    env: group options

--- skynet/env.py
+++ skynet/env.py
@@ -3,13 +3,21 @@ import sys
 
 is_mac = sys.platform == 'darwin'
 
+# models
 llama_path = os.environ.get('LLAMA_PATH')
+llama_n_gpu_layers = int(os.environ.get('LLAMA_N_GPU_LAYERS', 1 if is_mac else 40))
+llama_n_batch = int(os.environ.get('LLAMA_N_BATCH', 512))
+
 
 # auth
 bypass_auth = os.environ.get('BYPASS_AUTHORIZATION', "False").lower() == 'true'
 asap_pub_keys_url = os.getenv('ASAP_PUB_KEYS_REPO_URL', None)
 asap_pub_keys_folder = os.getenv('ASAP_PUB_KEYS_FOLDER', None)
 asap_pub_keys_auds = os.getenv('ASAP_PUB_KEYS_AUDS', '').strip().split(',')
+
+if not bypass_auth and not asap_pub_keys_url:
+    raise RuntimeError('The ASAP public keys repo url must be set')
+
 
 # redis
 redis_exp_seconds = int(os.environ.get('REDIS_EXP_SECONDS', 60 * 30))  # 30 minutes default
@@ -24,11 +32,6 @@ redis_use_secrets_manager = os.environ.get('REDIS_USE_SECRETS_MANAGER', 'false')
 redis_namespace = os.environ.get('REDIS_NAMESPACE', 'skynet')
 redis_aws_region = os.environ.get('REDIS_AWS_REGION', 'us-west-2')
 
+
 # monitoring
 enable_metrics = os.environ.get('ENABLE_METRICS', 'true').lower() == 'true'
-
-if not bypass_auth and not asap_pub_keys_url:
-    raise RuntimeError('The ASAP public keys repo url must be set')
-
-llama_n_gpu_layers = int(os.environ.get('LLAMA_N_GPU_LAYERS', 1 if is_mac else 40))
-llama_n_batch = int(os.environ.get('LLAMA_N_BATCH', 512))
