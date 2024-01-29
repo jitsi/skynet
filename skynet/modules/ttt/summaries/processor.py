3d2517471e2ea3ada73400519bf5cf051e04077f
Avram Tudor <tudor.potecaru@8x8.com>
2024-01-29 15:35:59 +0200

    chore: update llama and langchain

--- skynet/modules/ttt/summaries/processor.py
+++ skynet/modules/ttt/summaries/processor.py
@@ -1,10 +1,11 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
-from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain_core.documents import Document
 
 from skynet.env import llama_n_batch, llama_n_gpu_layers, llama_path
 
@@ -22,12 +23,16 @@ def initialize():
 
     llm = LlamaCpp(
         model_path=llama_path,
-        temperature=0.01,
+        temperature=0,
         max_tokens=4096,
         n_ctx=4096,
         n_gpu_layers=llama_n_gpu_layers,
         n_batch=llama_n_batch,
     )
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
 async def process(job: Job) -> str:
@@ -38,11 +43,12 @@ async def process(job: Job) -> str:
 
     loop = asyncio.get_running_loop()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=12000, chunk_overlap=100)
-    docs = text_splitter.create_documents([text])
+    docs = [Document(page_content=text)] if len(text) < 12000 else text_splitter.create_documents([text])
     template = summary_template if job.type is JobType.SUMMARY else action_items_template
 
     prompt = PromptTemplate(template=template, input_variables=["text"])
 
-    chain = load_summarize_chain(llm, chain_type="map_reduce", combine_prompt=prompt)
+    # once langchain adds support for LCEL map reduce, update this by splitting the use cases for large and small docs
+    chain = {"text": format_docs} | prompt | llm
 
-    return await loop.run_in_executor(executor, chain.run, docs)
+    return await loop.run_in_executor(executor, chain.invoke, docs)
