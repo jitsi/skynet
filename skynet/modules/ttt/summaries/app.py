c51f4a0a87ce305d4695556c47c824f1286283d4
Avram Tudor <tudor.potecaru@8x8.com>
2024-03-01 12:02:51 +0200

    ref: create fast api app only for summaries:dispatcher

--- skynet/modules/ttt/summaries/app.py
+++ skynet/modules/ttt/summaries/app.py
@@ -13,17 +13,19 @@ from .v1.router import router as v1_router
 
 log = get_logger(__name__)
 
-app = FastAPI()
-app.include_router(v1_router)
 
-Versionizer(app=app, prefix_format='/v{major}', sort_routes=True).versionize()
+async def dispatcher_startup():
+    app = FastAPI()
+    app.include_router(v1_router)
 
+    Versionizer(app=app, prefix_format='/v{major}', sort_routes=True).versionize()
 
-async def app_startup():
     log.info('summaries:dispatcher module initialized')
 
     await db.initialize()
     log.info('Persistence initialized')
+
+    return app
 
 
 async def executor_startup():
