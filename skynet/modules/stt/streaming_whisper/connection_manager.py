651ce76f1b74c5b8e99643f4345c0c5db58e17bf
Răzvan Purdel <104128753+rpurdel@users.noreply.github.com>
2024-03-22 09:38:46 +0200

    Streaming whisper flush worker (#65)
    * feat: mute button for streaming-whisper demo
    
    * fix: solve a corner case when the user has some untranscribed audio which gets merged with the next after a very long time
    
    * fix: make constant, start flush task lazily
    
    * fix: put constant out of class

--- skynet/modules/stt/streaming_whisper/connection_manager.py
+++ skynet/modules/stt/streaming_whisper/connection_manager.py
@@ -1,4 +1,6 @@
+import asyncio
 import time
+from asyncio import Task
 
 from fastapi import WebSocket, WebSocketDisconnect
 
@@ -11,12 +13,16 @@ from skynet.modules.stt.streaming_whisper.utils import utils
 
 log = get_logger(__name__)
 
+FLUSH_AFTER_MS = 2000
+
 
 class ConnectionManager:
     connections: dict[str, MeetingConnection]
+    flush_audio_task: Task | None
 
     def __init__(self):
         self.connections: dict[str, MeetingConnection] = {}
+        self.flush_audio_task = None
 
     async def connect(self, websocket: WebSocket, meeting_id: str, auth_token: str | None):
         if not bypass_auth:
@@ -26,6 +32,9 @@ class ConnectionManager:
                 return
         await websocket.accept()
         self.connections[meeting_id] = MeetingConnection(websocket)
+        if self.flush_audio_task is None:
+            loop = asyncio.get_running_loop()
+            self.flush_audio_task = loop.create_task(self.flush_working_audio_worker())
         CONNECTIONS_METRIC.set(len(self.connections))
         log.info(f'Meeting with id {meeting_id} started. Ongoing meetings {len(self.connections)}')
 
@@ -39,18 +48,18 @@ class ConnectionManager:
         end = time.perf_counter_ns()
         processing_time = (end - start) / 1e6 / 1000
         TRANSCRIBE_DURATION_METRIC.observe(processing_time)
+        await self.send(meeting_id, results)
+
+    async def send(self, meeting_id: str, results: list[utils.TranscriptionResponse] | None):
         if results is not None:
             for result in results:
-                await self.send(meeting_id, result)
-
-    async def send(self, meeting_id: str, result: utils.TranscriptionResponse):
-        try:
-            await self.connections[meeting_id].ws.send_json(result.model_dump())
-        except WebSocketDisconnect as e:
-            log.warning(f'Meeting {meeting_id}: the connection was closed before sending all results: {e}')
-            self.disconnect(meeting_id)
-        except Exception as ex:
-            log.error(f'Meeting {meeting_id}: exception while sending transcription results {ex}')
+                try:
+                    await self.connections[meeting_id].ws.send_json(result.model_dump())
+                except WebSocketDisconnect as e:
+                    log.warning(f'Meeting {meeting_id}: the connection was closed before sending all results: {e}')
+                    self.disconnect(meeting_id)
+                except Exception as ex:
+                    log.error(f'Meeting {meeting_id}: exception while sending transcription results {ex}')
 
     def disconnect(self, meeting_id: str):
         try:
@@ -58,3 +67,22 @@ class ConnectionManager:
         except KeyError:
             log.warning(f'The meeting {meeting_id} doesn\'t exist anymore.')
         CONNECTIONS_METRIC.set(len(self.connections))
+
+    async def flush_working_audio_worker(self):
+        """
+        Will force a transcription for all participants that haven't received any chunks for more than `flush_after_ms`
+        but have accumulated some spoken audio without a transcription. This avoids merging un-transcribed "left-overs"
+        to the next utterance when the participant resumes speaking.
+        """
+        while True:
+            for meeting_id in self.connections:
+                for participant in self.connections[meeting_id].participants:
+                    now = utils.now()
+                    last_received_chunk = self.connections[meeting_id].participants[participant].last_received_chunk
+                    is_due = now - last_received_chunk > FLUSH_AFTER_MS
+                    is_silent, _ = utils.is_silent(self.connections[meeting_id].participants[participant].working_audio)
+                    if is_due and not is_silent:
+                        log.info(f'Forcing a transcription in meeting {meeting_id} for {participant}')
+                        results = await self.connections[meeting_id].participants[participant].force_transcription()
+                        await self.send(meeting_id, results)
+            await asyncio.sleep(1)
