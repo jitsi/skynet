e82a362943aad146dabeed3593b581fd8c280c4c
Avram Tudor <tudor.potecaru@8x8.com>
2024-06-04 15:38:56 +0300

    feat: move openai api to separate process altogether

--- docker/run-skynet.sh
+++ docker/run-skynet.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd /app
 . .venv/bin/activate
+
+if [[ ${ENABLED_MODULES:="summaries:dispatcher,summaries:executor"} == *"summaries:executor"* ]]; then
+  /app/llama.cpp/server \
+    -m ${LLAMA_PATH} \
+    -b ${LLAMA_N_BATCH:=512} \
+    -c ${LLAMA_N_CTX:=8192} \
+    -ngl ${LLAMA_N_GPU_LAYERS:=40} \
+    --port ${OPENAI_API_SERVER_PORT:=8002} & \
+fi
+
 exec python3.11 skynet/main.py
+
+wait -n
--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 cd llama.cpp
 make server
 cd ..
 
+DEFAULT_GPU_LAYERS=40
+
+if [ "$(uname)" == "Darwin" ]; then
+  DEFAULT_GPU_LAYERS=-1
+fi
+
+if [[ ${ENABLED_MODULES:="summaries:dispatcher,summaries:executor"} == *"summaries:executor"* ]]; then
+  ${OPENAI_API_SERVER_PATH} \
+    -m ${LLAMA_PATH} \
+    -b ${LLAMA_N_BATCH:=512} \
+    -c ${LLAMA_N_CTX:=8192} \
+    -ngl ${LLAMA_N_GPU_LAYERS:=${DEFAULT_GPU_LAYERS}} \
+    --port ${OPENAI_API_SERVER_PORT:=8002} & \
+fi
+
 poetry run python -m uvicorn skynet.main:app --reload
+
+wait -n
