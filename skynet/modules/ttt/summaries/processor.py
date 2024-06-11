97785c0093c1dd7c8a3ccc2211d18b601fddec43
Avram Tudor <tudor.potecaru@8x8.com>
2024-06-11 16:00:36 +0300

    fix(summaries) optimize thresholds for map reduce summarisation (#86)

--- skynet/modules/ttt/summaries/processor.py
+++ skynet/modules/ttt/summaries/processor.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from langchain_openai import ChatOpenAI
 
-from skynet.env import app_uuid, openai_api_base_url
+from skynet.env import app_uuid, llama_n_ctx, openai_api_base_url
 from skynet.logs import get_logger
 
 from .prompts.action_items import action_items_conversation_prompt, action_items_text_prompt
@@ -12,7 +12,6 @@ from .prompts.summary import summary_conversation_prompt, summary_text_prompt
 from .v1.models import DocumentPayload, HintType, JobType
 
 llm = None
-map_reduce_threshold = 12000
 log = get_logger(__name__)
 
 
@@ -51,11 +50,23 @@ async def process(payload: DocumentPayload, job_type: JobType, model: ChatOpenAI
     system_message = hint_type_to_prompt[job_type][payload.hint]
     prompt = ChatPromptTemplate.from_messages([("system", system_message), ("user", "{text}")])
 
-    if len(text) < map_reduce_threshold:
+    # this is a rough estimate of the number of tokens in the input text, since llama models will have a different tokenization scheme
+    num_tokens = current_model.get_num_tokens(text)
+
+    # allow some buffer for the model to generate the output
+    threshold = llama_n_ctx * 3 / 4
+
+    if num_tokens < threshold:
         chain = load_summarize_chain(current_model, chain_type="stuff", prompt=prompt)
         docs = [Document(page_content=text)]
     else:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=map_reduce_threshold, chunk_overlap=100)
+        # split the text into roughly equal chunks
+        num_chunks = num_tokens // threshold + 1
+        chunk_size = num_tokens // num_chunks
+
+        log.info(f"Splitting text into {num_chunks} chunks of {chunk_size} tokens")
+
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=100)
         docs = text_splitter.create_documents([text])
         chain = load_summarize_chain(current_model, chain_type="map_reduce", combine_prompt=prompt, map_prompt=prompt)
 
