b1db6e1b1fa8e79188f4a84b0cd6f74159f67705
Avram Tudor <tudor.potecaru@8x8.com>
2024-08-26 08:26:18 +0000

    fix: update summaries prompts
    Should result in less suppositions (e.g. "the conversation appears to be a meeting").

--- skynet/modules/ttt/summaries/prompts/summary.py
+++ skynet/modules/ttt/summaries/prompts/summary.py
@@ -1,13 +1,17 @@
 summary_conversation_prompt = """
-    Summarize the following conversation. Start your response with "Response:".
+    You are an AI assistant that will be provided a conversation transcript. You will extract a summary of that transcript.
     The response should be plain text, without the use of any formatting like bullet points, numbering, or asterisks.
+    Start your response with "Response:".
 
+    Text:
     {text}
 """
 
 summary_text_prompt = """
-    Summarize the following text. Start your response with "Response:".
+    You are an AI assistant that will be provided a text transcript. You will extract a summary of that transcript.
     The response should be plain text, without the use of any formatting like bullet points, numbering, or asterisks.
+    Start your response with "Response:".
 
+    Text:
     {text}
 """
