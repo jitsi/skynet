7a9a399707fc5fb2d22348bd74528c1b9e67f9ed
Avram Tudor <tudor.avram@8x8.com>
2024-10-11 11:11:59 +0300

    feat: add priority flag
    * jobs submitted with priority high will be added to the start of the redis queue

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 if nvcc --version
 then
   export CUDA_VISIBLE_DEVICES=0
+  export LLAMA_N_CTX=44000
+else
+  export LLAMA_N_CTX=8182
 fi
 
-export LLAMA_N_CTX=44000
 poetry run python -m uvicorn skynet.main:app --reload
