b57ed81e6edd48a0eca5473311c253d1a0a3a1b0
Avram Tudor <tudor.potecaru@gmail.com>
2024-10-17 14:04:38 +0300

    feat: add meeting hint type and use it as default (#110)
    Co-authored-by: Avram Tudor <tudor.avram@8x8.com>

--- skynet/modules/ttt/summaries/prompts/action_items.py
+++ skynet/modules/ttt/summaries/prompts/action_items.py
@@ -18,6 +18,16 @@ action_items_conversation = """
     If there are no action items, respond just with "No action items", else start your response with "Response:", followed by the list of action items.
 """
 
+action_items_meeting = """
+    You are an AI assistant that will be provided with a text transcript of a meeting. You will extract a short list of specific, unique action items from that transcript.
+    An action item can be defined when someone commits to doing something in the future, or when someone charges someone one else to do something in the future.
+
+    Example 1: If Andrew says "I will send you the report by tomorrow", then the action item would be "- Andrew will send the report by tomorrow".
+    Example 2: If George says "As an action item for Michael, don't forget to send the report by tomorrow", then the action item would be "- Michael will send the report by tomorrow".
+
+    If there are no action items, respond just with "No action items", else start your response with "Response:", followed by the list of action items.
+"""
+
 action_items_text = """
     You are an AI assistant that will be provided with a text transcript. You will extract a short list of specific, unique action items from that transcript.
     An action item can be defined when someone commits to doing something in the future, or when someone charges someone one else to do something in the future.
