22237df71340947f6dddd6661b61c907688e38d0
Avram Tudor <tudor.potecaru@gmail.com>
2024-10-25 08:44:12 +0300

    feat: expose openai api endpoints from vllm (#112)
    * feat: expose openai api endpoints from vllm
    
    * code review changes
    
    
    Co-authored-by: Avram Tudor <tudor.avram@8x8.com>

--- skynet/modules/ttt/openai_api/app.py
+++ skynet/modules/ttt/openai_api/app.py
@@ -1,4 +1,7 @@
+import asyncio
 import subprocess
+
+from fastapi import FastAPI
 
 from skynet import http_client
 from skynet.env import (
@@ -9,37 +12,52 @@ from skynet.env import (
     llama_path,
     openai_api_base_url,
     openai_api_server_port,
-    vllm_server_path,
+    use_vllm,
 )
 from skynet.logs import get_logger
-from skynet.utils import get_device
-
-proc = None
-use_vllm = get_device() == 'cuda'
+from skynet.utils import dependencies, responses
 
 log = get_logger(__name__)
 
 
-def initialize():
+async def run_vllm_server(args, app: FastAPI):
+    from vllm.entrypoints.openai.api_server import build_async_engine_client, init_app_state, router
+
+    async with build_async_engine_client(args) as engine_client:
+        app.include_router(router, dependencies=dependencies, responses=responses)
+
+        model_config = await engine_client.get_model_config()
+        init_app_state(engine_client, model_config, app.state, args)
+
+
+def initialize(app: FastAPI | None = None):
     log.info('Starting OpenAI API server...')
 
-    global proc
-
     if use_vllm:
-        openai_api_server_path = vllm_server_path
-        proc = subprocess.Popen(
-            f'python -m {openai_api_server_path} \
-                --disable-log-requests \
-                --model {llama_path} \
-                --gpu_memory_utilization 0.99 \
-                --max-model-len {llama_n_ctx} \
-                --port {openai_api_server_port}'.split(),
-            shell=False,
+        from vllm.entrypoints.openai.cli_args import make_arg_parser
+        from vllm.utils import FlexibleArgumentParser
+
+        parser = FlexibleArgumentParser(description="vLLM OpenAI-Compatible RESTful API server.")
+        parser = make_arg_parser(parser)
+        args = parser.parse_args(
+            [
+                '--disable-frontend-multiprocessing',  # disable running the engine in a separate process
+                '--disable-log-requests',
+                '--model',
+                llama_path,
+                '--gpu_memory_utilization',
+                '0.99',
+                '--max-model-len',
+                str(llama_n_ctx),
+                '--port',
+                str(openai_api_server_port),
+            ]
         )
+
+        asyncio.create_task(run_vllm_server(args, app))
     else:
-        openai_api_server_path = llama_cpp_server_path
-        proc = subprocess.Popen(
-            f'{openai_api_server_path} \
+        subprocess.Popen(
+            f'{llama_cpp_server_path} \
                 --batch-size {llama_n_batch} \
                 --ctx-size {llama_n_ctx} \
                 --flash-attn \
@@ -49,25 +67,17 @@ def initialize():
             shell=False,
         )
 
-    if proc.poll() is not None:
-        log.error(f'Failed to start OpenAI API server from {openai_api_server_path}')
-    else:
-        log.info(f'OpenAI API server started from {openai_api_server_path}')
-
 
 async def is_ready():
     try:
-        await http_client.get(f'{openai_api_base_url}/health', 'text' if use_vllm else 'json')
+        response = await http_client.get(f'{openai_api_base_url}/health', 'text' if use_vllm else 'json')
+
+        if use_vllm:
+            return response == ''
 
         return True
     except Exception:
         return False
 
 
-def destroy():
-    log.info('Killing OpenAI API subprocess...')
-
-    proc.kill()
-
-
-__all__ = ['destroy', 'initialize', 'restart']
+__all__ = ['initialize', 'is_ready']
