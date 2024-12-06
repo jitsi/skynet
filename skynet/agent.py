c15fcfa7b4b081110b6975962b1719a1fafdf350
RazvanP <razvan.purdel@8x8.com>
2024-12-06 15:56:41 +0200

    feat: adds rest api to drain node for autoscaling

--- skynet/agent.py
+++ skynet/agent.py
@@ -1,12 +1,18 @@
 import asyncio
 import math
-
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 from skynet.env import whisper_max_connections
 
 from skynet.logs import get_logger
 from skynet.modules.monitoring import CONNECTIONS_METRIC
 
 log = get_logger(__name__)
+app = FastAPI()
+
+STATE_FILE = '/tmp/streaming-whisper-connections-state'
+haproxy_state = 'ready'
+haproxy_check_state_task = None
 
 
 async def calc_percentage():
@@ -19,9 +25,9 @@ async def calc_percentage():
 
 
 async def handle_tcp_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    global haproxy_state
     percentage = await calc_percentage()
-    status = 'up ready '
-    response = f'{status}{percentage}%\n'
+    response = f'up {haproxy_state} {percentage}%\n'
     if writer is not None:
         try:
             writer.write(response.encode())
@@ -41,3 +47,68 @@ async def create_tcpserver(port):
         await tcpserver.serve_forever()
     except KeyboardInterrupt:
         tcpserver.close()
+
+
+# Endpoints for the autoscaler to query the current state of the system
+class StateResponse(BaseModel):
+    request_status: str
+    current_state: str
+    desired_state: str
+
+
+class CurrentStateResponse(BaseModel):
+    current_state: str
+    active_connections: int
+
+
+async def get_current_state():
+    try:
+        with open(STATE_FILE, 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        log.error(f'The state file does not exist')
+        with open(STATE_FILE, 'w') as f:
+            f.write('ready')
+        return 'ready'
+    except Exception as e:
+        log.error(f'Failed to read the state file: {e}')
+        return 'unknown'
+
+
+@app.post('/state')
+async def echo_requests(request: Request):
+    global haproxy_check_state_task
+    payload = await request.json()
+    log.info(f'Received state change request: {payload}')
+    current_state = await get_current_state()
+
+    if haproxy_check_state_task is None:
+        loop = asyncio.get_running_loop()
+        haproxy_check_state_task = loop.create_task(update_haproxy_state())
+
+    if payload['state'] and payload['state'] in ['drain', 'maint', 'ready']:
+        with open(STATE_FILE, 'w') as f:
+            f.write(payload['state'].strip())
+        return StateResponse(request_status='success',
+                                   current_state=current_state,
+                                   desired_state=payload['state'])
+
+    log.error(f'Invalid state change request: {payload}')
+    return StateResponse(request_status='failed', current_state=current_state, desired_state=payload['state'])
+
+
+@app.get('/state')
+async def echo_requests(request: Request):
+    return CurrentStateResponse(current_state=await get_current_state(),
+                                active_connections=CONNECTIONS_METRIC._value.get())
+
+
+# Update the HAProxy state if it changes via the API
+async def update_haproxy_state():
+    global haproxy_state
+    while True:
+        new_state = await get_current_state()
+        if new_state != haproxy_state:
+            log.info(f'HAProxy state changed from {haproxy_state} to {new_state}')
+            haproxy_state = new_state
+        await asyncio.sleep(1)
