58053ad158f6b161152b8a8ca0dcc1f6fa7b25be
Saúl Ibarra Corretgé <saghul@jitsi.org>
2025-02-07 16:45:21 +0100

    feat: make vllm optional

--- sync-requirements.sh
+++ sync-requirements.sh
@@ -2,3 +2,7 @@
 
 poetry export --without-hashes --format=requirements.txt > requirements.txt
 git add requirements.txt
+
+poetry export --with vllm --without-hashes --format=requirements.txt > requirements-vllm.txt
+git add requirements-vllm.txt
+
