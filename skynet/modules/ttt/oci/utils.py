f664c630299f1a11eb3b4ba142052dfd8aad6aaf
Saúl Ibarra Corretgé <saghul@jitsi.org>
2025-02-20 11:45:40 +0100

    fix(oci) add initialization step
    Run a small generation to make sure we have downloaded the cached
    transformers config.

---
+++ skynet/modules/ttt/oci/utils.py
@@ -0,0 +1,40 @@
+from langchain_community.chat_models import ChatOCIGenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+from skynet.env import (
+    oci_auth_type,
+    oci_compartment_id,
+    oci_config_profile,
+    oci_model_id,
+    oci_service_endpoint,
+    use_oci,
+)
+
+
+async def initialize():
+    if not use_oci:
+        return
+
+    # Prime it so all transformers config is downloaded.
+    model_kwargs = {
+        "temperature": 0,
+        "frequency_penalty": 1,
+        "max_tokens": None,
+    }
+
+    llm = ChatOCIGenAI(
+        model_id=oci_model_id,
+        service_endpoint=oci_service_endpoint,
+        compartment_id=oci_compartment_id,
+        provider="meta",
+        model_kwargs=model_kwargs,
+        auth_type=oci_auth_type,
+        auth_profile=oci_config_profile,
+    )
+
+    messages = [
+        SystemMessage(content="Your are an AI assistant."),
+        HumanMessage(content="Hello."),
+    ]
+
+    await llm.ainvoke(messages)
