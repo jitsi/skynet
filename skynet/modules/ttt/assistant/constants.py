6ea7b72b537671c3b342be39f10607dc77690814
Avram Tudor <tudor.avram@8x8.com>
2025-02-20 08:07:50 +0200

    tweak prompts

--- skynet/modules/ttt/assistant/constants.py
+++ skynet/modules/ttt/assistant/constants.py
@@ -1,6 +1,6 @@
-assistant_default_system_message = "You are an AI assistant. You provide helpful suggestions"
+assistant_default_system_message = "You are an AI assistant who provides the next answer in a given conversation."
 assistant_rag_question_extractor = """
-    Based on the provided text, formulate a proper question for using with RAG.
+    Based on the provided text, formulate a proper question for RAG.
     Start your response with "Response:".
 """
-assistant_limit_data_to_rag = "Respond only based on the information provided above."
+assistant_limit_data_to_rag = "Only respond based on the information provided below."
