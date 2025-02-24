371aa697a005fc9286370ee40bd175e827797ebe
RazvanP <razvan.purdel@8x8.com>
2025-02-24 14:29:21 +0200

    fix: do not ignore pydantic models

---
+++ skynet/modules/stt/shared/models/whisper.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import List
+
+class WhisperWord(BaseModel):
+    probability: float
+    word: str
+    start: float
+    end: float
+
+class WhisperSegment(BaseModel):
+    id: int
+    seek: int
+    start: float
+    end: float
+    text: str
+    tokens: List[int]
+    temperature: float
+    avg_logprob: float
+    compression_ratio: float
+    no_speech_prob: float
+    words: List
+
+class WhisperResult:
+    text: str
+    segments: list[WhisperSegment]
+    words: list[WhisperWord]
+    confidence: float
+    language: str
+
+    def __init__(self, ts_result):
+        self.text = ''.join([segment.text for segment in ts_result])
+        self.segments = [WhisperSegment.model_validate(segment._asdict()) for segment in ts_result]
+        self.words = [WhisperWord.model_validate(word._asdict()) for segment in ts_result for word in segment.words]
+        self.confidence = self.get_confidence()
+
+    def __str__(self):
+        return (
+            f'Text: {self.text}\n'
+            + f'Confidence avg: {self.confidence}\n'
+            + f'Segments: {self.segments}\n'
+            + f'Words: {self.words}'
+        )
+
+    def get_confidence(self) -> float:
+        if len(self.words) > 0:
+            return float(sum(word.probability for word in self.words) / len(self.words))
+        return 0.0
