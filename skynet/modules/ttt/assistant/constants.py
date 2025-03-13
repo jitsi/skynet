52cffdb1913636a8e375838b09c1907b0c544666
Avram Tudor <tudor.avram@8x8.com>
2025-03-13 17:47:23 +0200

    fix: outline more clearly the rag support docs

--- skynet/modules/ttt/assistant/constants.py
+++ skynet/modules/ttt/assistant/constants.py
@@ -5,4 +5,4 @@ assistant_rag_question_extractor = f"""
     Based on the provided text, formulate a proper question for RAG.
     Start your response with "{response_prefix}".
 """
-assistant_limit_data_to_rag = "Only respond based on the information provided below."
+assistant_limit_data_to_rag = "Only respond based on the information provided in the documents above."
